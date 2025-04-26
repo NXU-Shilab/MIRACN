6fae3bd517cfca19491b178f71ac54ec3e3f49e6
Gungnir021 <118533739+Gungnir021@users.noreply.github.com>
2025-04-26 16:12:10 +0800

    Update GTEx_Append.py

--- Data/GTEx/GTEx_Append.py
+++ Data/GTEx/GTEx_Append.py
@@ -1,3 +1,22 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:da460f2652bdec30dcb214afbdefed386568b5668834ab8b85ab800d1d37944d
-size 835
+import pandas as pd
+
+# Read the label.txt file
+label_df = pd.read_csv('label.txt', sep='\t', header=None)
+
+# Read the GTEx-variant-data_tabix.csv file
+gtex_df = pd.read_csv('GTEx-variant-data_tabix.csv', header=None)
+
+# Check if the number of rows in both is the same
+print(len(gtex_df))
+print(len(label_df))
+if len(gtex_df) != len(label_df):
+    raise ValueError("The number of rows in GTEx-variant-data_tabix.csv and label.txt files is not the same")
+
+# Set the column names of label_df to prevent conflicts with gtex_df column names
+label_df.columns = [f'label_{i+1}' for i in range(label_df.shape[1])]
+
+# Append the columns in label_df to the last three columns of gtex_df
+result_df = pd.concat([gtex_df, label_df], axis=1)
+
+# Save the new CSV file
+result_df.to_csv('GTEx-variant-data_tabix_label.csv', index=False)
