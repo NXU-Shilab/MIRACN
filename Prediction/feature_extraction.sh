f701294a7e97dd1c0e40aad49191eaf1adb98827
Gungnir021 <118533739+Gungnir021@users.noreply.github.com>
2025-04-26 16:35:11 +0800

    Update feature_extraction.sh

--- Prediction/feature_extraction.sh
+++ Prediction/feature_extraction.sh
@@ -1,3 +1,2 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0a77b1d7c79c7904a9dc0af75751bb336251594207a16da9ef29a0c43215e86c
-size 59
+columns=$(cat ./feature.txt)
+cut -f "$columns" "$1" > "$2"
