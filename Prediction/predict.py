e03a9127d0a2b64667007628b1ad4da43f3c78b0
Gungnir021 <118533739+Gungnir021@users.noreply.github.com>
2025-04-26 16:37:30 +0800

    Update predict.py

--- Prediction/predict.py
+++ Prediction/predict.py
@@ -1,3 +1,36 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:56de4ca090e7929a4a19fa6861a22f88a706f86332500918ee04cb69bf4e6b36
-size 1337
+import argparse
+import pandas as pd
+import numpy as np
+from keras.models import load_model
+
+# Create an argument parser
+parser = argparse.ArgumentParser(description='Load data, make predictions, and save results.')
+parser.add_argument('file_name', type=str, help='Path to the data file')
+args = parser.parse_args()
+
+# Load data
+data = pd.read_csv(args.file_name, sep='\t')
+X_test = data.iloc[:, 5:]
+
+# Load model
+model_path = 'MIRACN.h5'
+loaded_model = load_model(model_path)
+
+# Make predictions using the loaded model
+X_test_fold = np.array(X_test)
+
+y_Functionality_Proba = loaded_model.predict(X_test_fold)[0]
+y_Cell_Line_Proba = loaded_model.predict(X_test_fold)[1]
+
+output_data = data.iloc[:, :5]
+
+output_data['Functionality_Probability'] = y_Functionality_Proba
+
+classes = ["GM12878", "GM18507", "HaCaT", "HEK293FT", "HEK293T", "HepG2", "K562"]
+
+for i, class_name in enumerate(classes):
+    output_data[class_name] = y_Cell_Line_Proba[:, i]
+
+output_file_name = args.file_name.replace('.csv', '_predictions.csv')
+output_data.to_csv(output_file_name, index=False, sep='\t')
+print(f'Predicted probabilities saved to {output_file_name}')
