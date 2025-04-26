47ef244c267534161621a6b7d022bdaf10dfcfd5
Gungnir021 <118533739+Gungnir021@users.noreply.github.com>
2025-04-26 16:11:29 +0800

    Update GTEx_Merge.py

--- Data/GTEx/GTEx_Merge.py
+++ Data/GTEx/GTEx_Merge.py
@@ -1,3 +1,21 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:72713b7ca070d4e921c12b92ce4c10ea2aa04f5a782e023c4a40692a7301a92e
-size 818
+import pandas as pd
+
+# Read the first file (GTEx_use_label_deleted_38.tsv)
+file1 = '../independent_test_GTEx/GTEx-variant-data_38_replaced_with_header.tsv'
+df1 = pd.read_csv(file1, sep='\t')
+
+# Create a join key for the first file
+df1['key'] = df1.iloc[:, 0].str.replace('chr', '') + '_' + df1.iloc[:, 1].astype(str)
+
+# Read the second file (result_GTEx_38.csv)
+file2 = '../end_product/result_GTEx_38_rm_train.csv'
+df2 = pd.read_csv(file2, sep=',')
+
+# Remove the 'chr' prefix from the first column and create a join key
+df2['key'] = df2.iloc[:, 0].str.replace('chr', '') + '_' + df2.iloc[:, 1].astype(str)
+
+# Merge the two DataFrames
+merged_df = pd.merge(df1, df2, on='key')
+
+# Save the merged DataFrame to a new CSV file
+merged_df.to_csv('../end_product/merged_result_GTEx_38_rm_train.csv', index=False)
