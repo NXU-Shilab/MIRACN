cdc1e1da3f7458af2ce15a0e6358a7c46f02b3d8
Gungnir021 <118533739+Gungnir021@users.noreply.github.com>
2025-04-26 16:19:17 +0800

    Update Expecto_Pred.py

--- Data/MPRA/ExPecto/Expecto_Pred.py
+++ Data/MPRA/ExPecto/Expecto_Pred.py
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:16e660580ef6f7bb66b38d210ab65882d86418382491d7eb07b77cabf1286863
-size 411
+import pandas as pd
+
+# Read the CSV file
+df = pd.read_csv('my_ExPecto/output_l_1.csv')
+
+# Get the data from the 12th column to the last column
+data = df.iloc[:, 12:]
+
+# Check if any absolute value is greater than 0.3
+df['pred'] = (data.abs() > 0.3).any(axis=1)
+
+# Convert True and False to 1 and 0
+df['pred'] = df['pred'].astype(int)
+
+# Save the result to a new CSV file
+df.to_csv('expecto_pred.csv', index=False)
